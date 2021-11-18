1ec66555a8d5861e38865957288692d2ceda027e
BrentonPedler <brenton_pedler@hotmail.com>
2021-11-17 21:16:54 -0500

    added firebase to react app

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// Firebase.js
+
+import firebase from 'firebase/firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCUWcC2wCaYMOrKb8XGsy31eS0RnpdvQaM",
+
+    authDomain: "brentonpedlerprojectthree.firebaseapp.com",
+
+    projectId: "brentonpedlerprojectthree",
+
+    storageBucket: "brentonpedlerprojectthree.appspot.com",
+
+    messagingSenderId: "475276929119",
+
+    appId: "1:475276929119:web:e1ebf0a283d406139a53ea"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
