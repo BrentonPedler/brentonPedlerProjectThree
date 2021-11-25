20d5fd73c577a0f6c28641ccede7e8687b6f84d4
BrentonPedler <brenton_pedler@hotmail.com>
2021-11-24 20:24:45 -0500

    indentation update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // APP.js
-
 import './App.css';
 import Header from './Header';
 import Form from './Form';
@@ -9,42 +8,27 @@ import firebase from './firebase.js';
 
 
 function App() {
-
   const [quotes, setQuotes] = useState([]);
-
-
   // DATA FROM FIREBASE
-
   useEffect(() => {
-
     // REFERENCE TO DATA
-
     const dbRef = firebase.database().ref();
-
     dbRef.on('value', (response) => {
-
       // VARIABLE TO STORE NEW STATE
-
       const newState = [];
       const data = response.val();
-
       for (let property in data) {
         const obj = {
           property,
           ...data[property]
-          }
-
+        }
         newState.push(obj);
       }
-
       setQuotes(newState)
-
     })
   }, [])
 
-
   return (
-
     <div className="App">
       <Header />
       <main className="wrapper">
@@ -55,7 +39,5 @@ function App() {
     
   );
 }
-
-
 
 export default App;
